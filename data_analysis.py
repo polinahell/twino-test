f0ebb46967b985c4f409f2cd21bdfdade1540bbd
polinahell <polinaartemchik@gmail.com>
2023-11-15 23:36:39 +0200

    initial commit

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn import metrics
 
 def volume_of_deals_over_time(df, date_column):
     """Calculate and visualize the volume of deals over time."""
